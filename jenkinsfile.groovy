786aefb85a773e1dc6d7e6916682f65cfa033220
root <root@ip-172-31-91-24.ec2.internal>
2022-02-23 20:32:50 +0000

    added free

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,10 +1,10 @@
 node {
-    stage ('BUild-VPC'){
+    stage ('BUild-VPC-free'){
         timestamps {
             echo "hello world!-101"
         }
     }
-    stage ('Build-RDS'){
+    stage ('Build-RDS-free'){
     }
     echo "l think l am okay with jenkins-101"
     stage ('Result'){
