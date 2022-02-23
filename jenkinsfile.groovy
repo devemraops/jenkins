30fd3aa45c42a343806d255205924d039884f411
root <root@ip-172-31-91-24.ec2.internal>
2022-02-23 21:02:10 +0000

    added default things

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,15 +1,26 @@
+
 node {
-    stage ('BUild-VPC-free'){
-        timestamps {
-            echo "hello world!-101"
+    def mvnHome
+    stage('Preparation') { // for display purposes
+        // Get some code from a GitHub repository
+        git 'https://github.com/jglick/simple-maven-project-with-tests.git'
+        // Get the Maven tool.
+        // ** NOTE: This 'M3' Maven tool must be configured
+        // **       in the global configuration.
+        mvnHome = tool 'M3'
+    }
+    stage('Build') {
+        // Run the maven build
+        withEnv(["MVN_HOME=$mvnHome"]) {
+            if (isUnix()) {
+                sh '"$MVN_HOME/bin/mvn" -Dmaven.test.failure.ignore clean package'
+            } else {
+                bat(/"%MVN_HOME%\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+            }
         }
     }
-    stage ('Build-RDS-free'){
+    stage('Results') {
+        junit '**/target/surefire-reports/TEST-*.xml'
+        archiveArtifacts 'target/*.jar'
     }
-    echo "l think l am okay with jenkins-101"
-    stage ('Result'){
-    }
-    stage ('notification-slack-101'){
-    }
-
 }
